8e01dd5174144487c47264a1f0ea4c723636a2f2
fishel <ren_feixiang@126.com>
2017-08-28 20:15:36 +0800

    json_demo modify

--- python-demos/json_demo.py
+++ python-demos/json_demo.py
@@ -33,15 +33,14 @@ separators=(item_separator, key_separator)去除分隔符后的空格；
 skipkeys为True，则字典的键不是基本类型（str, int, float, bool, None）时会被跳过，
 而不是抛出异常
 '''
-
-'''
-#  3.自定义类型的编码与解码
 class Person(object):
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
-
+p = Person("Jack", 22)
+'''
+#  3.自定义类型的编码与解码
 def convert_to_builtin_type(obj):
     d = {
         '__class__': obj.__class__.__name__,
@@ -52,11 +51,8 @@ def convert_to_builtin_type(obj):
     # object.__dict__ 用于存储对象属性的字典
     return d
 
-
-p = Person("Jack", 22)
 print(json.dumps(p, default=convert_to_builtin_type))
 # default 用于指定将自定义类型转换为可以序列化为JSON字符串的Python内置类型的函数
-
 
 def dict_to_object(d):
     if '__class__' in d:
@@ -69,9 +65,66 @@ def dict_to_object(d):
         obj = d
     return obj
 
-
 encoded_object = '{"__class__": "Person", "__module__": "__main__", "name": "Jack", "age": 22}'
 newObj=json.loads(encoded_object, object_hook=dict_to_object)
 print(type(newObj))
 '''
 
+'''
+# JSONEncoder.iterencode方法将对象的各个部分分别进行编码
+encoder=json.JSONEncoder()
+data = [ { 'a':'A', 'b':(2, 4), 'c':3.0 } ]
+for part in encoder.iterencode(data):
+    print('PART:'+part)
+'''
+'''
+PART:[
+PART:{
+PART:"a"
+PART:: 
+PART:"A"
+PART:, 
+PART:"b"
+PART:: 
+PART:[2
+PART:, 4
+PART:]
+PART:, 
+PART:"c"
+PART:: 
+PART:3.0
+PART:}
+PART:]
+'''
+
+class MyEncoder(json.JSONEncoder): # 自定义编码器
+    def default(self, obj):
+        d = {
+            '__class__': obj.__class__.__name__,
+            '__module__': obj.__module__
+        }
+        d.update(obj.__dict__)
+        # dict.update 用其它字典或可迭代的键值对来升级当前字典，重写已经存在的键
+        # object.__dict__ 用于存储对象属性的字典
+        return d
+
+result=json.dumps(p,cls=MyEncoder)
+print(result)
+
+class MyDecoder(json.JSONDecoder):
+    def __init__(self):
+        json.JSONDecoder.__init__(self,object_hook=self.dict_to_object)
+
+    def dict_to_object(self,d):
+        if '__class__' in d:
+            class_name = d.pop('__class__')  # dict.pop 删除键并返回对应的值
+            module_name = d.pop('__module__')
+            module = importlib.import_module(module_name)  # 导入一个模块
+            class_ = getattr(module, class_name)
+            obj = class_(**d)
+        else:
+            obj = d
+        return obj
+t='{"__class__": "Person", "__module__": "__main__", "name": "Jack", "age": 22}'
+r=MyDecoder().decode(t)
+print(r)
